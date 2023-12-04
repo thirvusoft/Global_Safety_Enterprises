175896dbbf08a7b3e040482cbf972348fefc588f
creative-paramu <pparameshwari@thirvusoft.in>
2023-12-04 12:32:39 +0530

    lead_customer_connection

--- global_safety_enterprises/global_safety_enterprises/utils/js/quotation.js
+++ global_safety_enterprises/global_safety_enterprises/utils/js/quotation.js
@@ -3,6 +3,7 @@ frappe.ui.form.on("Quotation", {
     refresh: function(frm){
 
 		setTimeout(() => {
+			frm.remove_custom_button("Set as Lost");
 			frm.remove_custom_button('Sales Order',"Create");
 			frm.remove_custom_button("Opportunity", "Get Items From");
 		}, 100)
