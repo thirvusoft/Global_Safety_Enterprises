0ce448fa9e08391875303f658ba935083ac6983e
Gokulnath17 <gokulnath@thirvusoft.in>
2023-12-05 09:51:55 +0000

    live site changes

--- global_safety_enterprises/global_safety_enterprises/utils/js/lead.js
+++ global_safety_enterprises/global_safety_enterprises/utils/js/lead.js
@@ -25,21 +25,16 @@ frappe.ui.form.on("Lead", {
 			};
 		});
 
-		var opp_value = await frappe.db.get_list("Opportunity",
-			{
-				filters:{party_name: cur_frm.doc.name}
-			}
-		)
+		if (!frm.doc.__islocal){
+		
+			frm.add_custom_button(__('<p style="color: #171717; padding-top:8px;padding-left:10px;padding-right:10px;"><b>Create Opportunity</b></p>'), () => {
 
-	{
-		frm.add_custom_button(__('<p style="color: #171717; padding-top:8px;padding-left:10px;padding-right:10px;"><b>Create Opportunity</b></p>'), () => {
-
-			frappe.model.open_mapped_doc({
-				method: "erpnext.crm.doctype.lead.lead.make_opportunity",
-				frm: frm
+				frappe.model.open_mapped_doc({
+					method: "erpnext.crm.doctype.lead.lead.make_opportunity",
+					frm: frm
+				});
+				
 			});
-			
-		});
 		}
     },
 	onload:function(frm){
