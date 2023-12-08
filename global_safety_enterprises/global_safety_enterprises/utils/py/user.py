73a3e9b63751d7826722ed1e405dcb8880feb8b7
Barath <barath@thirvusoft.in>
2023-12-08 14:35:38 +0530

    User Permission Delete

--- global_safety_enterprises/global_safety_enterprises/utils/py/user.py
+++ global_safety_enterprises/global_safety_enterprises/utils/py/user.py
@@ -27,6 +27,9 @@ def user_permission_create(doc, action=None):
             user_permission_check = frappe.db.get_value("Role Profile", doc.role_profile_name, "custom_user_permission")
             if not user_permission_check:
                 frappe.delete_doc("User Permission", user_permission_list[0].name)
-            
-            
         
+            if  doc.missed_followup or doc.followup_report:
+                user_permission_list = frappe.get_all("User Permission", filters={"user":doc.name, "allow": "User", 
+                                "for_value":doc.name, "apply_to_all_doctypes":1}, fields=["name", "user", "for_value"])
+                for i in user_permission_list:
+                        frappe.delete_doc("User Permission", i.name)
