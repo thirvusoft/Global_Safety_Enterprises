209a24d92edb9cd88c653630554c068347202f88
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2024-01-26 17:49:46 +0530

    slack notification

--- global_safety_enterprises/__init__.py
+++ global_safety_enterprises/__init__.py
@@ -1,3 +1,12 @@
 
 __version__ = '0.0.1'
+import erpnext, frappe
+from erpnext.selling.doctype.quotation.quotation import set_expired_status
+from global_safety_enterprises.global_safety_enterprises.utils.py.quotation import set_expired_status_global
 
+def ses(*a, **b):
+    if 'global_safety_enterprises' in frappe.get_installed_apps():
+        return set_expired_status_global(*a, **b)
+    return set_expired_status(*a, **b)
+
+erpnext.selling.doctype.quotation.quotation.set_expired_status = ses
